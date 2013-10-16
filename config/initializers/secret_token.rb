c71c09de53373110d32f768f33ea6b71974bcbec
Josh Nankin <josh@phaxio.com>
2013-10-16 15:39:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhaxioApiproxy::Application.config.secret_key_base = 'd626fae1f8c130cb2243dbfa4bc0abee8bc1d9703d0b6ae54d59ffeecefb54c96daf48a46ea2edc0d09bc85e55cec088e4055331c160d55065e7865c7f1f7e13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhaxioApiproxy::Application.config.session_store :cookie_store, key: '_phaxio-apiproxy_session'
